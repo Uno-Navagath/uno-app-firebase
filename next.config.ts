6e8a25b7d251b1c72f816252d46b017bb7fdc290
Ramees <ramees.main@gmail.com>
2025-08-31 19:12:24 +0530

    Image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                port: '',
+                pathname: '/seed/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+            }
+        ]
+    },
 };
 
 export default nextConfig;
